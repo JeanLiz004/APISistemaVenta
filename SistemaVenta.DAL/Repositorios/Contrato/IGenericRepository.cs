39418b7d8234948419f07e7e065f67361751f687
JeanLiz004 <eljeanliz@hotmail.com>
2024-04-15 12:14:12 -0400

    Agregar archivos de proyecto.

---
+++ SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        //Metodos que obtenga TModel y devuelva booleano
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        Task<TModel> Crear(TModel modelo);
+        //Devuelve booleano
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eliminar(TModel modelo);
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null);
+    }
+}
